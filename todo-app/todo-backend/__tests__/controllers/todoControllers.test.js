0c42251e8d4ea5b3f887d49979c88e81e5c1152d
anil <anil@gmail.com>
2025-04-27 17:07:08 +0530

    test cases added

--- todo-app/todo-backend/__tests__/controllers/todoControllers.test.js
+++ todo-app/todo-backend/__tests__/controllers/todoControllers.test.js
@@ -1,6 +1,5 @@
 const todoControllers = require("../../controllers/todoControllers") 
 
-// jest.mock("../../models/todoModels.js")
 jest.mock("../../models/todoModels.js")
 
 const mockSave = jest.fn();
@@ -41,33 +40,3 @@ describe("when Todo Controller is Invoked", () => {
         });
     });
 });
-
-
-// describe("when Todo Controller is invoked",()=>{
-//     let req,res;
-//     beforeEach(()=>{
-//         req={
-//             body:{},
-//             params:{}
-//         };
-//         res={
-//             json:jest.fn(()=>res),
-//             status:jest.fn(()=>res),
-//         }
-//     })
-// })
-
-// describe("when Todo Controller is Invoked", () =>{
-//     describe("For getTodos function", () =>{
-//         it("should return me all the todos",async ()=>{
-//             const mockTodos = [{_id:0,title:"Todo 1",completed: false},{_id:1,title:"Todo 2",completed: false},{_id:3,title:"Todo 3",completed: false}]
-//             mockFind.mockResolvedValue(mockTodos)
-//             await todoControllers.getTodos(req,res);
-
-//             expect(mockFind).toHaveBeenCalled()
-//             expect(res.status).toHaveBeenCalledWith(200);
-//             expect(res.json).toHaveBeenCalledWith(mockTodos)
-
-//         })
-//     })
-// })
