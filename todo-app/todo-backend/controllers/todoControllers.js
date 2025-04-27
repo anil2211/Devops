a123af7ede753f880af8ffd6454edb72d6e75da8
anil <anil@gmail.com>
2025-04-27 20:42:09 +0530

    test cases added remaing addtodo

--- todo-app/todo-backend/controllers/todoControllers.js
+++ todo-app/todo-backend/controllers/todoControllers.js
@@ -1,28 +1,37 @@
 const Todo =require("../models/todoModels")
-
+const logger = require("../utils/loggers")
 
 exports.getTodos= async (req,res)=>{
     console.log("Fetching the todos from the DBs")
     try {
         const todos= await Todo.find();
-        console.log("fetched all the todos",todos)
+        // log.debug()
+        logger.info("fetched all the todos",todos)
+        // console.log("fetched all the todos",todos)
         res.status(200).json(todos)
     } catch (error) {
-        console.log("Error while fetching the todos",error)
+        logger.error("Error while fetching the todos",error)
         res.status(500).json({message:"something went wrong..."})
     }
 }
 
 exports.addTodo = async(req,res)=>{
+    try {
+        
     const title=req.body;
     // console.log("Adding a new todo",req.body)
-    console.log("adding a new todo",title.todo)
+    logger.info("adding a new todo",title.todo)
     const newTodo = new Todo({
         title:title.todo
     })
-    console.log("Adding the todo to DB",newTodo)
+    logger.info("Adding the todo to DB",newTodo)
     const savedTodo = await newTodo.save()
-    console.log("Added the todo to Db",savedTodo)
+    logger.info("Added the todo to Db",savedTodo)
 
     res.status(200).json(savedTodo)
+    } catch (error) {
+        logger.error("Error while adding  the todos",error)
+        res.status(500).json({message:"something went wrong..."})
+    }
+    
 }
